996b580b98ee4202f33d5560165947330eccbffb
mogita <me@mogita.com>
2019-01-15 18:27:54 +0800

    Add statuses destroy API

--- fanfou/statuses_test.go
+++ fanfou/statuses_test.go
@@ -87,3 +87,37 @@ func TestStatusesService_Show(t *testing.T) {
 		t.Errorf("statuses.show returned %+v, want %+v", status, want)
 	}
 }
+
+func TestStatusesService_Destroy(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/statuses/destroy.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		_, err := fmt.Fprint(w, `{"id": "test_id", "in_reply_to_status_id": "test1", "in_reply_to_user_id": "test2", "repost_status_id": "test3", "source": "test4", "location": "test7"}`)
+		if err != nil {
+			t.Errorf("statuses.destroy mock server error: %+v", err)
+		}
+	})
+
+	status, err := client.Statuses.Destroy("test_id", &StatusesOptParams{
+		Mode:   "test5",
+		Format: "test6",
+	})
+	if err != nil {
+		t.Errorf("statuses.destroy returned error: %v", err)
+	}
+
+	want := &Status{
+		ID:                "test_id",
+		InReplyToStatusID: "test1",
+		InReplyToUserID:   "test2",
+		RepostStatusID:    "test3",
+		Source:            "test4",
+		Location:          "test7",
+	}
+
+	if !reflect.DeepEqual(status, want) {
+		t.Errorf("statuses.destroy returned %+v, want %+v", status, want)
+	}
+}
