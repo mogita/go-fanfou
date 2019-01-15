0bfda417a094914ff95a4354ba302cfe7336d8c1
mogita <me@mogita.com>
2019-01-16 00:44:33 +0800

    Add cancel recommendation API

--- fanfou/users_test.go
+++ fanfou/users_test.go
@@ -209,3 +209,37 @@ func TestUsersService_Recommendation(t *testing.T) {
 		t.Errorf("users.recommendation returned %+v, want %+v", users, want)
 	}
 }
+
+func TestUsersService_CancelRecommendation(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/2/users/cancel_recommendation.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		_, err := fmt.Fprint(w, `{"id": "test_id", "name": "test1", "screen_name": "test2", "location": "test3", "gender": "test4", "profile_image_url": "test7"}`)
+		if err != nil {
+			t.Errorf("users.cancel_recommendation mock server error: %+v", err)
+		}
+	})
+
+	user, err := client.Users.CancelRecommendation("test_id", &UsersOptParams{
+		Mode:   "test5",
+		Format: "test6",
+	})
+	if err != nil {
+		t.Errorf("users.cancel_recommendation returned error: %v", err)
+	}
+
+	want := &User{
+		ID:              "test_id",
+		Name:            "test1",
+		ScreenName:      "test2",
+		Location:        "test3",
+		Gender:          "test4",
+		ProfileImageURL: "test7",
+	}
+
+	if !reflect.DeepEqual(user, want) {
+		t.Errorf("users.cancel_recommendation returned %+v, want %+v", user, want)
+	}
+}
