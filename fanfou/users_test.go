ce0d51ba464b12cb30f8a2c11912320814d1c3fc
mogita <me@mogita.com>
2019-01-16 00:05:24 +0800

    Add users tagged API

---
+++ fanfou/users_test.go
@@ -0,0 +1,54 @@
+package fanfou
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestUsersService_Tagged(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/users/tagged.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		_, err := fmt.Fprint(w, `[{"id": "test_id", "name": "test1", "screen_name": "test2", "location": "test3", "gender": "test4", "profile_image_url": "test7"},{"id": "test_id", "name": "test1", "screen_name": "test2", "location": "test3", "gender": "test4", "profile_image_url": "test7"}]`)
+		if err != nil {
+			t.Errorf("users.tagged mock server error: %+v", err)
+		}
+	})
+
+	users, err := client.Users.Tagged("test_tag", &UsersOptParams{
+		Page:   1,
+		Count:  1,
+		Mode:   "test5",
+		Format: "test6",
+	})
+	if err != nil {
+		t.Errorf("users.tagged returned error: %v", err)
+	}
+
+	want := []User{
+		{
+			ID:              "test_id",
+			Name:            "test1",
+			ScreenName:      "test2",
+			Location:        "test3",
+			Gender:          "test4",
+			ProfileImageURL: "test7",
+		},
+		{
+			ID:              "test_id",
+			Name:            "test1",
+			ScreenName:      "test2",
+			Location:        "test3",
+			Gender:          "test4",
+			ProfileImageURL: "test7",
+		},
+	}
+
+	if !reflect.DeepEqual(users, want) {
+		t.Errorf("users.tagged returned %+v, want %+v", users, want)
+	}
+}
