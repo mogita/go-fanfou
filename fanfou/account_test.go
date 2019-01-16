49d4c0c6922d02e19b494ad2af586ba6deca5fb7
mogita <me@mogita.com>
2019-01-17 00:51:42 +0800

    Add account rate limit status API

--- fanfou/account_test.go
+++ fanfou/account_test.go
@@ -40,3 +40,32 @@ func TestAccountService_VerifyCredentials(t *testing.T) {
 		t.Errorf("account.verify_credentials returned %+v, want %+v", user, want)
 	}
 }
+
+func TestAccountService_RateLimitStatus(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/account/rate_limit_status.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		_, err := fmt.Fprint(w, `{"reset_time": "Mon Nov 14 08:57:28 +0000 2011", "remaining_hits": 150, "hourly_limit": 150, "reset_time_in_seconds": 1321261048}`)
+		if err != nil {
+			t.Errorf("account.rate_limit_status mock server error: %+v", err)
+		}
+	})
+
+	result, err := client.Account.RateLimitStatus()
+	if err != nil {
+		t.Errorf("account.rate_limit_status returned error: %v", err)
+	}
+
+	want := &RateLimitStatusResult{
+		ResetTime:          "Mon Nov 14 08:57:28 +0000 2011",
+		RemainingHits:      150,
+		HourlyLimit:        150,
+		ResetTimeInSeconds: 1321261048,
+	}
+
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("account.rate_limit_status returned %+v, want %+v", result, want)
+	}
+}
