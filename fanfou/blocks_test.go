d3073b3e7ebb8aef2e564081cabc13b3ea3fbcab
mogita <me@mogita.com>
2019-01-16 23:42:05 +0800

    Add blocks blocking API

--- fanfou/blocks_test.go
+++ fanfou/blocks_test.go
@@ -34,3 +34,48 @@ func TestBlocksService_IDs(t *testing.T) {
 		t.Errorf("blocks.ids returned %+v, want %+v", users, want)
 	}
 }
+
+func TestBlocksService_Blocking(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/blocks/blocking.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		_, err := fmt.Fprint(w, `[{"id": "test_id", "name": "test1", "screen_name": "test2", "location": "test3", "gender": "test4", "profile_image_url": "test7"},{"id": "test_id", "name": "test1", "screen_name": "test2", "location": "test3", "gender": "test4", "profile_image_url": "test7"}]`)
+		if err != nil {
+			t.Errorf("blocks.blocking mock server error: %+v", err)
+		}
+	})
+
+	users, err := client.Blocks.Blocking(&BlocksOptParams{
+		Page:  1,
+		Count: 1,
+		Mode:  "test5",
+	})
+	if err != nil {
+		t.Errorf("blocks.blocking returned error: %v", err)
+	}
+
+	want := []User{
+		{
+			ID:              "test_id",
+			Name:            "test1",
+			ScreenName:      "test2",
+			Location:        "test3",
+			Gender:          "test4",
+			ProfileImageURL: "test7",
+		},
+		{
+			ID:              "test_id",
+			Name:            "test1",
+			ScreenName:      "test2",
+			Location:        "test3",
+			Gender:          "test4",
+			ProfileImageURL: "test7",
+		},
+	}
+
+	if !reflect.DeepEqual(users, want) {
+		t.Errorf("blocks.blocking returned %+v, want %+v", users, want)
+	}
+}
