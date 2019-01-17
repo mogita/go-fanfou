671587fcd1746868da744836980233269a76b444
mogita <me@mogita.com>
2019-01-17 18:54:52 +0800

    Add favorites destroy API

--- fanfou/favorites_test.go
+++ fanfou/favorites_test.go
@@ -54,7 +54,7 @@ func TestFavoritesService_IDs(t *testing.T) {
 	}
 }
 
-func TestStatusesService_Create(t *testing.T) {
+func TestFavoritesService_Create(t *testing.T) {
 	setup()
 	defer teardown()
 
@@ -85,5 +85,39 @@ func TestStatusesService_Create(t *testing.T) {
 
 	if !reflect.DeepEqual(status, want) {
 		t.Errorf("favorites.create returned %+v, want %+v", status, want)
+	}
+}
+
+func TestFavoritesService_Destroy(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/favorites/destroy.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		_, err := fmt.Fprint(w, `{"id": "test_id", "in_reply_to_status_id": "test1", "in_reply_to_user_id": "test2", "repost_status_id": "test3", "source": "test4", "location": "test7"}`)
+		if err != nil {
+			t.Errorf("favorites.destroy mock server error: %+v", err)
+		}
+	})
+
+	status, err := client.Favorites.Destroy("test_id", &FavoritesOptParams{
+		Mode:   "test5",
+		Format: "test6",
+	})
+	if err != nil {
+		t.Errorf("favorites.destroy returned error: %v", err)
+	}
+
+	want := &StatusResult{
+		ID:                "test_id",
+		InReplyToStatusID: "test1",
+		InReplyToUserID:   "test2",
+		RepostStatusID:    "test3",
+		Source:            "test4",
+		Location:          "test7",
+	}
+
+	if !reflect.DeepEqual(status, want) {
+		t.Errorf("favorites.destroy returned %+v, want %+v", status, want)
 	}
 }
