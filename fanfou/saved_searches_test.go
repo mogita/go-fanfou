41b4c4dfce829879a236c35a188ff4a42f422205
mogita <me@mogita.com>
2019-01-17 18:06:50 +0800

    Add saved searches create API

--- fanfou/saved_searches_test.go
+++ fanfou/saved_searches_test.go
@@ -71,3 +71,32 @@ func TestSavedSearchesService_List(t *testing.T) {
 		t.Errorf("saved_searches.list returned %+v, want %+v", user, want)
 	}
 }
+
+func TestSavedSearchesService_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/saved_searches/create.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		_, err := fmt.Fprint(w, `{"id": 21071, "name": "fanfou|test", "query": "fanfou|test", "created_at": "Thu Nov 10 09:05:03 +0000 2011"}`)
+		if err != nil {
+			t.Errorf("saved_searches.show mock server error: %+v", err)
+		}
+	})
+
+	user, err := client.SavedSearches.Create("fanfou|test")
+	if err != nil {
+		t.Errorf("saved_searches.show returned error: %v", err)
+	}
+
+	want := &SavedSearchResult{
+		ID:        21071,
+		Name:      "fanfou|test",
+		Query:     "fanfou|test",
+		CreatedAt: "Thu Nov 10 09:05:03 +0000 2011",
+	}
+
+	if !reflect.DeepEqual(user, want) {
+		t.Errorf("saved_searches.show returned %+v, want %+v", user, want)
+	}
+}
