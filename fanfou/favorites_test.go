965069d9ec69f64bdd80f1b82df2d4aaaafbfd19
mogita <me@mogita.com>
2019-01-17 18:52:01 +0800

    Add favorites create API

--- fanfou/favorites_test.go
+++ fanfou/favorites_test.go
@@ -53,3 +53,37 @@ func TestFavoritesService_IDs(t *testing.T) {
 		t.Errorf("favorites.id returned %+v, want %+v", user, want)
 	}
 }
+
+func TestStatusesService_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/favorites/create.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		_, err := fmt.Fprint(w, `{"id": "test_id", "in_reply_to_status_id": "test1", "in_reply_to_user_id": "test2", "repost_status_id": "test3", "source": "test4", "location": "test7"}`)
+		if err != nil {
+			t.Errorf("favorites.create mock server error: %+v", err)
+		}
+	})
+
+	status, err := client.Favorites.Create("test_id", &FavoritesOptParams{
+		Mode:   "test5",
+		Format: "test6",
+	})
+	if err != nil {
+		t.Errorf("favorites.create returned error: %v", err)
+	}
+
+	want := &StatusResult{
+		ID:                "test_id",
+		InReplyToStatusID: "test1",
+		InReplyToUserID:   "test2",
+		RepostStatusID:    "test3",
+		Source:            "test4",
+		Location:          "test7",
+	}
+
+	if !reflect.DeepEqual(status, want) {
+		t.Errorf("favorites.create returned %+v, want %+v", status, want)
+	}
+}
