f22148b838d4a9dfc3baf604b3c8f3b63e744f17
mogita <me@mogita.com>
2019-01-17 23:29:29 +0800

    Add friendships show API

--- fanfou/friendships.go
+++ fanfou/friendships.go
@@ -15,13 +15,41 @@ type FriendshipsService struct {
 	client *Client
 }
 
-// FriendsOptParams specifies the optional params for friendships API
+// FriendshipsShowResult specifies Fanfou's friendship show structure
+type FriendshipsShowResult struct {
+	Relationship *RelationshipResult `json:"relationship,omitempty"`
+}
+
+type RelationshipResult struct {
+	Source *RelationshipItem `json:"source,omitempty"`
+	Target *RelationshipItem `json:"target,omitempty"`
+}
+
+type RelationshipItem struct {
+	ID                   string `json:"id,omitempty"`
+	ScreenName           string `json:"screen_name,omitempty"`
+	Following            string `json:"following,omitempty"`
+	FollowedBy           string `json:"followed_by,omitempty"`
+	NotificationsEnabled string `json:"notifications_enabled,omitempty"`
+	Blocking             string `json:"blocking,omitempty"`
+}
+
+// FriendshipsOptParams specifies the optional params for friendships API
 type FriendshipsOptParams struct {
 	ID     string
 	Page   int64
 	Count  int64
 	Mode   string
 	Format string
+}
+
+// FriendshipsShowOptParams specifies the params specifically for friendship
+// show API
+type FriendshipsShowOptParams struct {
+	SourceLoginName string
+	TargetLoginName string
+	SourceID        string
+	TargetID        string
 }
 
 // Create shall add the specified user as a friend (follow)
@@ -201,7 +229,7 @@ func (s *FriendshipsService) Accept(ID string, opt *FriendshipsOptParams) (*User
 }
 
 // Exists shall check if user A follows user B
-// ID represents the user ID
+// "userA" and "userB" can be either the user ID or login name
 //
 // Fanfou API docs: https://github.com/mogita/FanFouAPIDoc/wiki/friendships.exists
 func (s *FriendshipsService) Exists(userA, userB string) (bool, error) {
@@ -231,4 +259,43 @@ func (s *FriendshipsService) Exists(userA, userB string) (bool, error) {
 	}
 
 	return resultBool, nil
+}
+
+// Show shall get the friendship between two users
+// Note: Please either supply both a source and a target
+//
+// Fanfou API docs: https://github.com/mogita/FanFouAPIDoc/wiki/friendships.show
+func (s *FriendshipsService) Show(opt *FriendshipsShowOptParams) (*FriendshipsShowResult, error) {
+	u := fmt.Sprintf("friendships/show.json")
+	params := url.Values{}
+
+	if opt != nil {
+		if opt.SourceLoginName != "" {
+			params.Add("source_login_name", opt.SourceLoginName)
+		}
+		if opt.SourceID != "" {
+			params.Add("source_id", opt.SourceID)
+		}
+		if opt.TargetLoginName != "" {
+			params.Add("target_login_name", opt.TargetLoginName)
+		}
+		if opt.TargetID != "" {
+			params.Add("target_id", opt.TargetID)
+		}
+	}
+
+	u += "?" + params.Encode()
+
+	req, err := s.client.NewRequest(http.MethodGet, u, "")
+	if err != nil {
+		return nil, err
+	}
+
+	result := new(FriendshipsShowResult)
+	_, err = s.client.Do(req, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
